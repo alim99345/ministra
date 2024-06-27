2d5c27c3794ccc47a0238c65c253e3dec4b832ce
alim99345 <34473602+alim99345@users.noreply.github.com>
2024-06-27 15:46:52 +0500

    Update and rename ministra5.6.9.sh to ministra5.6.8.sh

--- ministra5.6.8.sh
+++ ministra5.6.8.sh
@@ -9,7 +9,7 @@ apt-get install net-tools -y
 VERSION="5.6.8"
 TIME_ZONE="Europe/Amsterdam" #
 mysql_root_password="test123456"
-repository="https://enabling-roots-identity-mt.trycloudflare.com/stalker"
+repository="https://manner-rising-st-voting.trycloudflare.com/stalker"
 
 # SET LOCALE TO UTF-8
 function setLocale {
@@ -49,23 +49,16 @@ apt-get install apache2 -y
 /etc/init.d/apache2 stop
 sleep 1
 
-apt-get -y install php7.0-geoip php7.0-intl php7.0-tidy php7.0-igbinary php7.0-msgpack php7.0-mcrypt php7.0-mbstring php7.0-zip memcached php7.0-memcached php7.0 php7.0-xml php7.0-gettext php7.0-soap php7.0-mysql php-pear nodejs libapache2-mod-php7.0 php7.0-curl php7.0-imagick php7.0-sqlite3 unzip
-update-alternatives --set php /usr/bin/php7.0
+apt-get -y install php5.6-geoip php5.6-intl php5.6-tidy php5.6-igbinary php5.6-msgpack php5.6-mcrypt php5.6-mbstring php5.6-zip memcached php5.6 php5.6-xml php5.6-gettext php5.6-soap php5.6-mysql php-pear nodejs libapache2-mod-php5.6 php5.6-curl php5.6-imagick php5.6-sqlite3 unzip
+update-alternatives --set php /usr/bin/php5.6
 
 sleep 2
 
 echo -e " \e[32mInstalling phing\e[0m"
 sleep 3
 pear channel-discover pear.phing.info
+#pear install -Z phing/phing-2.15.2
 pear install --alldeps phing/phing-2.15.2
-
-echo -e " \e[32minstalling npm 2.15.11\e[0m"
-sleep 3
-# Install NPM  2.15.11
-apt-get install npm -y
-npm config set strict-ssl false
-npm install -g npm@2.15.11
-# ln -s /usr/bin/nodejs /usr/bin/node
 
 echo -e " \e[32mSet the Server Timezone to EDT\e[0m"
 sleep 3
@@ -79,7 +72,6 @@ export DEBIAN_FRONTEND="noninteractive"
 echo "mysql-server mysql-server/root_password password $mysql_root_password" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $mysql_root_password" | sudo debconf-set-selections
 apt-get install -y mysql-server
-# sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
 sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 mysql -uroot -p$mysql_root_password -e "USE mysql; UPDATE user SET Host='%' WHERE User='root' AND Host='localhost'; DELETE FROM user WHERE Host != '%' AND User='root'; FLUSH PRIVILEGES;"
 mysql -uroot -p$mysql_root_password -e "create database stalker_db;"
@@ -91,22 +83,38 @@ mysql -ustalker -p1 -e "ALTER USER stalker IDENTIFIED WITH mysql_native_password
 
 echo 'sql_mode=""' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 echo 'default_authentication_plugin=mysql_native_password' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i 's/max_allowed_packet[[:space:]]= 16/max_allowed_packet      = 32/' /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql restart
 
-echo -e " \e[32mInstalling Ministra Portal $VERSION \e[0m"
+echo -e " \e[32mInstalling Stalker Portal $VERSION \e[0m"
 sleep 3
 cd /var/www/html/
-wget http://download.ministra.com/downloads/159934057961c4dfe9153ee02d7e3fb1/ministra-5.6.9.zip
-unzip ministra-5.6.9.zip
+wget https://manner-rising-st-voting.trycloudflare.com/stalker/ministra-5.6.8.zip
+unzip ministra-5.6.8.zip
+mv ministra-5.6.8 stalker_portal
 rm -rf *.zip
 
+#Patch Support MAG420/MAG322/MAG324 ON THE PORTAL VERSION 4.9.X
+cd /var/www/html/stalker_portal
+wget $repository/support.patch
+patch -p1 < support.patch
+sleep 1
 
-sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.0/apache2/php.ini
-ln -s /etc/php/7.0/mods-available/mcrypt.ini /etc/php/8.0/mods-available/
+sed -i "s/'modified!=' => ''/'modified!=' => time()/" /var/www/html/stalker_portal/server/administrator/add_itv.php
+sed -i "44i\    this.ntp_wait_time = 0;\n" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "46i\    this.clock_formats = {'12h': '{2}:{1} {3}', '24h': '{0}:{1}'};\n" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "s/main_menu.time.innerHTML = get_word('time_format')/main_menu.time.innerHTML = stb.clock_formats[stb.profile.clock_format]/" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "s/main_menu.date.innerHTML = get_word('date_format')/main_menu.date.innerHTML = stb.clock_formats[stb.profile.clock_format]/" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "s/stb.player.info.clock.innerHTML = get_word('time_format')/stb.player.info.clock.innerHTML = stb.clock_formats[stb.profile.clock_format]/" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "s/module.tv.clock_box.innerHTML = get_word('time_format')/module.tv.clock_box.innerHTML = stb.clock_formats[stb.profile.clock_format]/" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "685i\                if (\!this.profile.clock_format) {" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "686i\                    this.profile.clock_format = (get_word('time_format') && this.clock_formats[get_word('time_format')]) ? this.clock_formats[get_word('time_format')]: '24h';" /var/www/html/stalker_portal/c/xpcom.common.js
+sed -i "687i\                }\n" /var/www/html/stalker_portal/c/xpcom.common.js
+
+sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/5.6/apache2/php.ini
+ln -s /etc/php/5.6/mods-available/mcrypt.ini /etc/php/8.0/mods-available/
 phpenmod mcrypt
 a2enmod rewrite
-# apt-get purge libapache2-mod-php5filter > /dev/null
-
 
 cd /etc/apache2/sites-enabled/
 rm -rf *
@@ -119,26 +127,17 @@ rm -rf default
 wget $repository/default
 /etc/init.d/apache2 restart
 /etc/init.d/nginx restart
-sleep 1
-rm -rf /var/www/html/stalker_portal/admin/vendor
-cd /var/www/html/stalker_portal/admin
-wget $repository/vendor.tar
-tar -xvf vendor.tar
-sleep 1
-
-
-# Fix Smart Launcher Applications
-mkdir /var/www/.npm
-chmod 777 /var/www/.npm
 
 cd /var/www/html/stalker_portal/server
-wget -O custom.ini $repository/custom.ini
-cd
+wget $repository/custom.ini
+
+cd /var/www/html/stalker_portal/server
+sed -i -r 's|^(default_timezone =).*|\1'" $TIME_ZONE"'|' config.ini
+sed -i -r 's/^(default_locale =).*/\1 en_US.utf8/' config.ini
 
 cd /var/www/html/stalker_portal/deploy
-sed -i 's/apt-get -y install zlibc curl php-sqlite3 php-soap php-intl php-gettext php-memcache php-memcached php-curl php-mysql php-mcrypt php-tidy php-imagick php-geoip curl npm git zip unzip php-zip/apt-get -y install zlibc curl php7.0-sqlite3 php-soap php7.0-intl php7.0-gettext php7.0-memcache php7.0-memcached php7.0-curl php7.0-mysql php7.0-mcrypt php7.0-tidy php7.0-imagick php7.0-geoip curl npm git zip unzip php7.0-zip/' build.xml
-sed -i 's/php5enmod/phpenmod/g' build.xml
-sed -i 's/php5dismod/phpdismod/g' build.xml
+sed -i 's/composer.phar install/composer.phar install --version=1.9.1/g' build.xml
+sed -i 's/apt-get -y install php-soap php5-intl php-gettext php5-memcache php5-curl php5-mysql php5-tidy php5-imagick php5-geoip curl/apt-get -y install php5.6-soap php5.6-intl php5.6-gettext php5.6-memcache php5.6-curl php5.6-mysql php5.6-tidy php5.6-imagick php5.6-geoip curl/g' build.xml
 sudo phing
 sleep 1
 
@@ -148,8 +147,8 @@ echo ""
 echo -e " \e[0mDefault username is: \e[32madmin"
 echo -e " \e[0mDefault password is: \e[32m1"
 echo ""
-echo -e " \e[0mPORTAL WAN : \e[32mhttp://`wget -qO- http://ipecho.net/plain | xargs echo`/stalker_portal"
-echo -e " \e[0mPORTAL LAN : \e[32mhttp://`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`/stalker_portal"
+echo -e " \e[0mPORTAL WAN : \e[32mhttp://`wget -qO- http://ipecho.net/plain | xargs echo`/stalker_portal/server/administrator"
+echo -e " \e[0mPORTAL LAN : \e[32mhttp://`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`/stalker_portal/server/administrator"
 echo -e " \e[0mMysql User : \e[32mroot"
 echo -e " \e[0mMySQL Pass : \e[32m$mysql_root_password"
 echo ""
